c1ccf90833f4949fe3c7d28578f5b2cb890e308f
Ramon Asuncion <asuncionbatista@gmail.com>
2026-01-07 14:02:46 -0500

    Add memory panel

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,25 @@
 import { PanelUI } from './ui.js'
+import { MemoryUI } from './memory.js'
 
 const panel = new PanelUI()
+const memory = new MemoryUI()
 
 window.switchTab = (tab) => {
-    console.log(tab)
     panel.switchTab(tab)
+}
+
+window.switchSubTab = (tab) => {
+    memory.switchSubTab(tab)
 }
 
 document.querySelectorAll('.panel-tab').forEach((tab) => {
     tab.addEventListener('click', () => {
         switchTab(tab.id.replace('panel-', ''))
+    })
+})
+
+document.querySelectorAll('.memory-sub-tab').forEach((tab) => {
+    tab.addEventListener('click', () => {
+        switchSubTab(tab.id.replace('mem-tab-', ''))
     })
 })
