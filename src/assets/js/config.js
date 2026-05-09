f9add0165bc3d4005b618861e3d698399ec1e43b
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-08 22:05:01 -0400

    Add todo via nag

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,6 +2,6 @@ const AVATAR_URL = 'localhost:8003'
 
 export const CONFIG = {
     wsUrl: `ws://${AVATAR_URL}/ws`,
-    apiUrl: `http://${AVATAR_URL}/`, // TODO: change to /api
+    apiUrl: `http://${AVATAR_URL}/`, // TODO(d2a8): change to /api
     reconnectTimer: 5000,
 }
