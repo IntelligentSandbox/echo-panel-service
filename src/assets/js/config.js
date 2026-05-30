f4d15e5c24a19ce1610f2caa76e4b8c152c3e847
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-30 11:06:31 -0400

    Update todos

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,6 +2,6 @@ const AVATAR_URL = 'localhost:8003'
 
 export const CONFIG = {
     wsUrl: `ws://${AVATAR_URL}/ws`,
-    apiUrl: `http://${AVATAR_URL}/`, // TODO(d2a8): change to /api
+    apiUrl: `http://${AVATAR_URL}/`,
     reconnectTimer: 5000,
 }
